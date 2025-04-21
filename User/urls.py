31d214d0812513776a8e2689766df6429c4a441d
Asilbek1-star <jasurfayzullaev85@gmail.com>
2025-04-21 21:03:08 +0800

    sa11

--- User/urls.py
+++ User/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
 
-urlpatterns = router.urls + [
+
+urlpatterns = [
     path('', views.home, name='home'),
     # Auth
     path('register/', views.register, name='register'),
