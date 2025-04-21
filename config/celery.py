bc239a5b69b4782b07321231193a4fab7d6f3d50
Asilbek1-star <jasurfayzullaev85@gmail.com>
2025-04-21 18:44:42 +0800

    say1

--- config/celery.py
+++ config/celery.py
@@ -2,14 +2,17 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Django settings modulini sozlash
+# Django project setting
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# Celery dasturini yaratish
 app = Celery('fransuz_tili_sayt')
 
-# Celery konfiguratsiyasini Django'niki bilan sozlash
+# Redis broker url
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Celery vazifalarini avtomatik ravishda topish
+app.conf.update(
+    broker_url='redis://redis:6379/0',  # Redis server URI
+    result_backend='redis://redis:6379/0',  # Celery task natijalari uchun
+)
+
 app.autodiscover_tasks()
